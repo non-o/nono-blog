01a8c862f9aad028495ee6b96e7186e73cdfa238
Cedric Dumont <cedric.dumont.ext@atos.net>
2016-04-01 12:01:53 +0200

    renamed folder common to menu and removed some comments in files

--- src/app/components/app.react.js
+++ src/app/components/app.react.js
@@ -1,29 +1,9 @@
 "use strict";
 import React from 'react';
 import { connect } from 'react-redux'
-import MenuListContainer from './common/menuListContainer';
+import MenuListContainer from './menu/menuListContainer';
 import PageContainer from './pages/pageContainer';
 import PostContainer from './post/postContainer';
-
-// const style = {
-//     menu: {
-//         display: 'inline-block',
-//         position: 'fixed',
-//         backgroundColor: '#888',
-//         float: 'left',
-//         width: 150,
-//         height: '100%',
-//         paddingLeft: 8,
-//         borderRightStyle: 'solid',
-//         borderRightColor: '#777',
-//         borderRightWidth: 2,
-//     },
-//     content: {
-//         display: 'inline-block',
-//         width: '100%',
-//         padding: '0 0 0 172'
-//     }
-// }
 
 class App extends React.Component {
 
@@ -40,7 +20,7 @@ class App extends React.Component {
         if (this.props.display == 'PAGE') {
             return (<PageContainer/>);
         }
-        else {
+        else { // for POST display but will display only one post
             return (<PostContainer/>);
         }
     }
